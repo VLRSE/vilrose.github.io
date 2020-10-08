9623c72da0168e627756de185e2f4c5fd037c3e2
rose <vcdaquiado@gmail.com>
2020-10-08 15:06:52 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(window).on("load", function(){
+	$(".loader .inner").fadeOut(300, function(){
+		$(".loader").fadeOut(550);
+	});
+})
+
+
+
+
+
+$(document).ready(function(){
+	//activate the superslide code
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000
+	});
+
+	var type = new Typed(".typed", {
+		strings: ["Aspiring Software Developer", "Autodidaktisch"],
+		typeSpeed: 70,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+	//Owl Carousel
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items:4,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+
+
+	var skillsToOffset = $(".skillsSection").offset().top;
+
+
+	$(window).scroll(function(){
+
+		if (window.pageYOffset > skillsToOffset - $(window).height() + 500){
+			$('.chart').easyPieChart({
+			    easing: 'easeInOut',
+				barColor: '#e67e22',
+				trackColor: false,
+				size:152,
+			
+				onStep: function(from, to, percent){
+					$(this.el).find('.percent').text(Math.round(percent));
+				} 
+			});
+		}
+
+	});
+
+	
+
+});
+
